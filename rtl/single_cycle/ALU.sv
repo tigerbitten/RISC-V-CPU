ed82ae41492199806afc4f2d2f6a2d368a19686a
William Stroud <william.edward.stroud@gmail.com>
2026-05-17 07:32:17 -0600

    added alu_control module

--- rtl/single_cycle/ALU.sv
+++ rtl/single_cycle/ALU.sv
@@ -2,9 +2,13 @@ module ALU(input [31:0]      a,
            input [31:0]      b,
            input [3:0]       alu_control,
            output reg [31:0] result,
-           output            zero);
+           output            zero,
+           output            negative,
+           output reg        overflow);
 
-    assign zero = (result == 0);
+    assign zero     = (result == 0);
+    assign negative = result[31];
+    assign overflow = (alu_control == 4'b0110) ? ((a[31] != b[31]) && (a[31] != result[31])) : (a[31] == b[31]) && (a[31] != result[31]); //compute overflow based off subtraction or addition operation
 
     always_comb begin
         case (alu_control)
---
+++ rtl/single_cycle/alu_control.sv
@@ -0,0 +1,24 @@
+module alu_control(input [1:0]      alu_op,
+                   input [2:0]      funct3,
+                   input            funct7_30,
+                   output reg [3:0] alu_control);
+    
+    always_comb begin
+        case (alu_op)
+          2'b00 : alu_control = 4'b0010;
+          2'b01 : alu_control = 4'b0110;
+          2'b10 : case ({funct3, funct7_30})
+                    4'b0000 : alu_control = 4'b0010; //ADD
+                    4'b0001 : alu_control = 4'b0110; //SUB
+                    4'b1110 : alu_control = 4'b0000; //AND
+                    4'b1100 : alu_control = 4'b0001; //OR
+                    4'b0010 : alu_control = 4'b0100; //SLL
+                    4'b0100 : alu_control = 4'b0111; //SLT
+                    4'b0110 : alu_control = 4'b1000; //SLTU
+                    4'b1000 : alu_control = 4'b0011; //XOR
+                    4'b1010 : alu_control = 4'b0101; //SRL
+                    4'b1011 : alu_control = 4'b1001; //SRA
+                  endcase
+        endcase
+    end
+endmodule
