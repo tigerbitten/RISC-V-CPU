0a468e9f0928e278afb195aed5bd7472bc341399
William Stroud <william.edward.stroud@gmail.com>
2026-05-27 00:29:45 -0600

    added riscv_pkg.sv for enumerated types/paramaters. created enum type for alu_control, implemented it in ALU.sv, and verified its correctness with ALU_tb.sv

--- rtl/single_cycle/ALU.sv
+++ rtl/single_cycle/ALU.sv
@@ -1,3 +1,5 @@
+import riscv_pkg::*;
+
 module ALU(input [31:0]      a,
            input [31:0]      b,
            input [3:0]       alu_control,
@@ -11,23 +13,23 @@ module ALU(input [31:0]      a,
     
     assign zero     = (result == 0);
     assign negative = result[31];
-    assign overflow = (alu_control == 4'b0110) ? ((a[31] != b[31]) && (a[31] != result[31])) : (a[31] == b[31]) && (a[31] != result[31]); //compute overflow based off subtraction or addition operation
+    assign overflow = (alu_control == ALU_SUB) ? ((a[31] != b[31]) && (a[31] != result[31])) : (a[31] == b[31]) && (a[31] != result[31]); //compute overflow based off subtraction or addition operation
     assign sub_ext  = {1'b0, a} - {1'b0, b};
     assign carry    = sub_ext[32];
 
     always_comb begin
         case (alu_control)
-          0  : result = a & b;
-          1  : result = a | b;
-          2  : result = a + b;
-          3  : result = a ^ b;
-          4  : result = a << b;
-          5  : result = a >> b;
-          6  : result = a - b;
-          7  : result = ($signed(a) < $signed(b)) ? 1 : 0;
-          8  : result = (a < b) ? 1 : 0;
-          9  : result = $signed(a) >>> b;
-          10 : result = b; //LUI -- simply passes b through to the output
+          ALU_AND  : result = a & b;
+          ALU_OR   : result = a | b;
+          ALU_ADD  : result = a + b;
+          ALU_XOR  : result = a ^ b;
+          ALU_SLL  : result = a << b;
+          ALU_SRL  : result = a >> b;
+          ALU_SUB  : result = a - b;
+          ALU_SLT  : result = ($signed(a) < $signed(b)) ? 1 : 0;
+          ALU_SLTU : result = (a < b) ? 1 : 0;
+          ALU_SRA  : result = $signed(a) >>> b;
+          ALU_LUI  : result = b; //LUI -- simply passes b through to the output
         endcase
     end
 endmodule
---
+++ rtl/single_cycle/riscv_pkg.sv
@@ -0,0 +1,16 @@
+package riscv_pkg;
+
+    typedef enum reg [3:0] {
+                            ALU_ADD  = 4'b0010,
+                            ALU_SUB  = 4'b0110,
+                            ALU_AND  = 4'b0000,
+                            ALU_OR   = 4'b0001,
+                            ALU_SLL  = 4'b0100,
+                            ALU_SLT  = 4'b0111,
+                            ALU_SLTU = 4'b1000,
+                            ALU_XOR  = 4'b0011,
+                            ALU_SRL  = 4'b0101,
+                            ALU_SRA  = 4'b1001,
+                            ALU_LUI  = 4'b1010
+                            } alu_control_t;
+    endpackage
