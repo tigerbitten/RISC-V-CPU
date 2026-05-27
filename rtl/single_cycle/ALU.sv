393389598cd6e9b35239e153d56713fc4013b9b9
William Stroud <william.edward.stroud@gmail.com>
2026-05-27 10:13:25 -0600

    optimized ALU.sv to synthesize less adders. everything should go through only one adder now. Still passes all tests in ALU_tb.sv

--- rtl/single_cycle/ALU.sv
+++ rtl/single_cycle/ALU.sv
@@ -9,28 +9,27 @@ module ALU(input [31:0]      a,
            output reg        overflow,
            output            carry);
 
-    wire [32:0] sub_ext; //33 bit temp subtraction for carry flag
+    wire [32:0] adder_out;
     
-    assign zero     = (result == 0);
-    assign negative = result[31];
-    assign overflow = (alu_control == ALU_SUB) ? ((a[31] != b[31]) && (a[31] != result[31])) : (a[31] == b[31]) && (a[31] != result[31]); //compute overflow based off subtraction or addition operation
-    assign sub_ext  = {1'b0, a} - {1'b0, b};
-    assign carry    = sub_ext[32];
+    assign zero      = (result == 0);
+    assign negative  = adder_out[31];
+    assign carry     = !adder_out[32];
+    
+    assign overflow  = (alu_control == ALU_SUB) ? ((a[31] != b[31]) && (a[31] != adder_out[31])) : (a[31] == b[31]) && (a[31] != adder_out[31]); //compute overflow based off subtraction or addition operation
+    assign adder_out = (alu_control == ALU_SUB || alu_control == ALU_SLT || alu_control == ALU_SLTU) ? {1'b0, a} + {1'b0, (~b)} + 1 : {1'b0, a} + {1'b0, b}; //2's complement addition for signals that need subtraction
 
     always_comb begin
         case (alu_control)
-          ALU_AND  : result = a & b;
-          ALU_OR   : result = a | b;
-          ALU_ADD  : result = a + b;
-          ALU_XOR  : result = a ^ b;
-          ALU_SLL  : result = a << b;
-          ALU_SRL  : result = a >> b;
-          ALU_SUB  : result = a - b;
-          ALU_SLT  : result = ($signed(a) < $signed(b)) ? 1 : 0;
-          ALU_SLTU : result = (a < b) ? 1 : 0;
-          ALU_SRA  : result = $signed(a) >>> b;
-          ALU_LUI  : result = b; //LUI -- simply passes b through to the output
+          ALU_AND           : result = a & b;
+          ALU_OR            : result = a | b;
+          ALU_ADD, ALU_SUB  : result = adder_out[31:0];
+          ALU_XOR           : result = a ^ b;
+          ALU_SLL           : result = a << b[4:0];
+          ALU_SRL           : result = a >> b[4:0];
+          ALU_SLT           : result = negative ^ overflow;
+          ALU_SLTU          : result = carry;
+          ALU_SRA           : result = $signed(a) >>> b[4:0];
+          ALU_LUI           : result = b; //LUI -- simply passes b through to the output
         endcase
     end
 endmodule
-           
