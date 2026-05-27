300f4bca30aa0cef911dc0316ca84ed0a32b6cd9
William Stroud <william.edward.stroud@gmail.com>
2026-05-27 03:23:36 -0600

    added opcode enum type to riscv_pkg and updated imm_gen.sv accordingly. Verified imm_gen_tb still passes all tests

--- rtl/single_cycle/imm_gen.sv
+++ rtl/single_cycle/imm_gen.sv
@@ -1,14 +1,16 @@
+import riscv_pkg::*;
+
 module imm_gen(input      [31:0] instruction,
                output reg [31:0] imm);
 
     always_comb begin
         case (instruction[6:0]) //decode type from opcode
-          7'b0010011, 7'b0000011, 7'b1100111 : imm = {{20{instruction[31]}}, instruction[31:20]}; //I-Type
-          7'b0100011 : imm = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};      //S-Type
-          7'b0110111, 7'b0010111 : imm = {instruction[31:12], 12'b000000000000};                  //U-Type
+          OP_ALU_IMM, OP_LOAD, OP_JALR : imm = {{20{instruction[31]}}, instruction[31:20]};
+          OP_STORE : imm = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
+          OP_LUI, OP_AUIPC : imm = {instruction[31:12], 12'b000000000000};
 
-          7'b1100011 : imm = {{20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0}; //B-Type
-          7'b1101111 : imm = {{12{instruction[31]}}, instruction[19:12], instruction[20], instruction[30:25], instruction[24:21], 1'b0}; //J-Type
+          OP_BRANCH : imm = {{20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0};
+          OP_JAL : imm = {{12{instruction[31]}}, instruction[19:12], instruction[20], instruction[30:25], instruction[24:21], 1'b0};
           default    : imm = 32'd0;
         endcase
     end
--- rtl/single_cycle/riscv_pkg.sv
+++ rtl/single_cycle/riscv_pkg.sv
@@ -20,5 +20,16 @@ package riscv_pkg;
                             ALUOP_RTYPE = 2'b10,
                             ALUOP_LUI   = 2'b11
                             } alu_op_t;
-    
+
+    typedef enum reg [6:0] {
+                            OP_RTYPE    = 7'b0110011,
+                            OP_ALU_IMM  = 7'b0010011, // (I-Type according to RISC-V)
+                            OP_LOAD     = 7'b0000011, // (I-Type according to RISC-V)
+                            OP_STORE    = 7'b0100011,
+                            OP_BRANCH   = 7'b1100011,
+                            OP_JAL      = 7'b1101111,
+                            OP_JALR     = 7'b1100111, // (I-Type according to RISC-V)
+                            OP_LUI      = 7'b0110111,
+                            OP_AUIPC    = 7'b0010111
+                            } opcode_t;
 endpackage
