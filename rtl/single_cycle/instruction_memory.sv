e9ab8c5fb0871232f2bd2e734feb4d6b7429b1b0
William Stroud <william.edward.stroud@gmail.com>
2026-05-30 09:35:14 -0600

    added paramater to instruction_memory.sv for which memory file to load as instruction memory. verified it is correct with testbench

--- rtl/single_cycle/instruction_memory.sv
+++ rtl/single_cycle/instruction_memory.sv
@@ -1,10 +1,12 @@
-module instruction_memory(input [31:0]  program_count,
-                          output [31:0] instruction);
+module instruction_memory #(parameter MEM_FILE = "instruction_mem_test.mem")
+    (input [31:0]  program_count,
+     output [31:0] instruction);
+    
     reg [31:0] memory [255:0];
     
     assign instruction = memory[program_count >> 2]; //divide by 4
 
     initial begin
-        $readmemh("instruction_mem_test.mem", memory);
+        $readmemh(MEM_FILE, memory);
     end
 endmodule
