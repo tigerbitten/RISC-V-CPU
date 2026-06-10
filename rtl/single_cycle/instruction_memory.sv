c5fb592cb51dec1eb07436ced4306d28bf457f69
William Stroud <william.edward.stroud@gmail.com>
2026-06-10 05:28:20 -0600

    expanded instruction_memory to 2048 32 bit entries for the purpose of using the riscv-tests test suite. updated its testbench accordingly and verified it still passes.

--- rtl/single_cycle/instruction_memory.sv
+++ rtl/single_cycle/instruction_memory.sv
@@ -2,7 +2,7 @@ module instruction_memory #(parameter MEM_FILE = "instruction_mem_test.mem")
     (input [31:0]  program_count,
      output [31:0] instruction);
     
-    reg [31:0] memory [255:0];
+    reg [31:0] memory [2047:0];
     
     assign instruction = memory[program_count >> 2]; //divide by 4
 
--- tb/single_cycle/instruction_memory_tb.sv
+++ tb/single_cycle/instruction_memory_tb.sv
@@ -1,23 +1,31 @@
 module instruction_memory_tb;
 
     reg [31:0] program_count, instruction;
+    integer    pass_count = 0;
+    integer    fail_count = 0;
     
-    instruction_memory DUT (.program_count (program_count),
-                            .instruction   (instruction));
+    instruction_memory #(.MEM_FILE("instruction_mem_test_2048.mem")) DUT (.program_count (program_count),
+                                                                          .instruction   (instruction));
     
     initial begin
-        for (int i = 0; i < 256; i++) begin
+        for (int i = 0; i < 2048; i++) begin
             program_count = i*4;
             
             #1;
             
             if (i !== instruction) begin
-                $display("FAIL: expected=%d instruction=%d pc=%b", i, instruction, program_count);
+                $display("FAIL: expected=%d instruction=%d pc=%h", i, instruction, program_count);
+                fail_count++;
             end else begin
-                $display("PASS: expected=%d instruction=%d pc_d=%d, pc_b=%b", i, instruction, program_count, program_count);
+                $display("PASS: expected=%d instruction=%d pc_d=%d, pc_h=%h", i, instruction, program_count, program_count);
+                pass_count++;
             end
         end
+        
+        $display("PASSED: %0d / 2048", pass_count);
+        $finish;
     end
     //instruction_mem_test.mem: each line counts upwards in hex from 0 to 255
-    //this testbench assumes instruction_mem_test.mem is the file begin loaded
+    //instruction_mem_test_2048.mem: each line counts upwards in hex from 0 to 2047
+    //this testbench assumes instruction_mem_test_2048.mem is the file begin loaded
 endmodule
