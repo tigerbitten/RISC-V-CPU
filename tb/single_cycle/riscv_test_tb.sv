eaf9bbebe202aee0b2c90dbcb5bc2cfe3a4025d1
William Stroud <william.edward.stroud@gmail.com>
2026-06-11 02:51:34 -0600

    working on building assembly programs, assembling them with the riscv-gnu-toolchain, and testing them with riscv_test_tb.sv. test_alu.mem passes, but need to automate assembly, conversion to .mem, and testing. and need to build out tests for other functions.

---
+++ tb/single_cycle/riscv_test_tb.sv
@@ -0,0 +1,48 @@
+module riscv_test_tb;
+       
+    parameter MEM_FILE = "test_alu.mem";
+    parameter CYCLES = 20;
+    
+    integer   pass_count = 0;
+    integer   fail_count = 0;
+    
+    reg clk, reset;
+    
+    cpu_top #(.MEM_FILE(MEM_FILE)) dut (.clk   (clk),
+                                        .reset (reset));
+    always begin
+        clk = 0;
+        #10;
+        clk = 1;
+        #10;
+    end
+
+    task tick();
+        begin
+            $display("PC:%d instruction:%h", dut.pc.pc_out, dut.imem.instruction);
+            @(posedge clk) #1;
+        end
+    endtask
+
+    initial begin
+        clk   = 0;
+        reset = 1;
+        tick(); //reset is SYNCHRONOUS so it needs a posedge
+        #10;
+        reset = 0;
+
+        repeat(CYCLES) tick();
+
+        if (riscv_test_tb.dut.reg_file.registers[3] == 32'd1) begin
+            $display("PASS: %s", MEM_FILE);
+            pass_count++;
+        end else begin
+            $display("FAIL: %s | registers[3] = %0d", MEM_FILE, riscv_test_tb.dut.reg_file.registers[3]);
+            fail_count++;
+        end
+        
+        $display("PASSED: %0d FAILED: %0d", pass_count, fail_count);
+        $finish;
+    end
+
+endmodule
